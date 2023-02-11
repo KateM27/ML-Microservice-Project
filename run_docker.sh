6f5fdd9d65578d08a6109858359bdc8de00fa94f
KateM27 <mamocatherine@gmail.com>
2023-02-11 20:48:18 +0300

    microsvc update

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker build --tag=ml-microsvc-k8 .
 
-# Step 2: 
 # List docker images
-
-# Step 3: 
+docker image ls
+ 
 # Run flask app
+docker run -p 8000:80 ml-microsvc-k8
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
+# This tags and uploads an image to Docker Hub 
 
-# Assumes that an image is built via `run_docker.sh`
-
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
-
-# Step 2:  
+dockerpath="katemamo/ml-microsvc-k8"
+ 
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u katemamo
+docker tag ml-microsvc-k8 $dockerpath
 
-# Step 3:
 # Push image to a docker repository
+docker push $dockerpath
